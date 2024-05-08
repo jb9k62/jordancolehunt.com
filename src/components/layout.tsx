02e23abfe45cbbc2836c5334a988bd2c75025558
jb9k62 <jb9k62@protonmail.com>
2024-05-08 10:53:40 +0200

    Add root layout component and update app to use layout

---
+++ src/components/layout.tsx
@@ -0,0 +1,27 @@
+// This is the root layout component for your Next.js app.
+// Learn more: https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts#root-layout-required
+
+import type { ReactNode } from "react";
+import { Chivo } from "next/font/google";
+import { Libre_Franklin } from "next/font/google";
+
+const chivo = Chivo({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-chivo",
+});
+const libre_franklin = Libre_Franklin({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-libre_franklin",
+});
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={chivo.variable + " " + libre_franklin.variable}>
+        {children}
+      </body>
+    </html>
+  );
+}
